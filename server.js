10350fa604d73dae125065d16cf2ce3edbba0a61
gtsaroj <sarojgt326@gmail.com>
2024-06-05 16:39:42 +0545

    testing

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const MissingdataRoute = require("./Routers/MissingRouter");
 const paymentRouter = require("./Routers/PaymentReciveRouter");
 const locationRouter = require("./Routers/LocationRouter");
 const liveDataRouter = require("./Routers/LiveDataRouter");
-const express = require("express")
+const express = require("express");
 const cors = require("cors");
 
 const server = express();
-server.use(cors({ origin: process.env.COR_URI }));
+server.use(cors({ origin: [process.env.COR_URI, "http://localhost:5173"], credentials: true }));
 
 server.use(express.json({ limit: "20kb" }));
 server.use(express.urlencoded({ extended: true, limit: "20kb" }));
@@ -19,5 +19,3 @@ server.use("/api", locationRouter);
 server.use("/api", liveDataRouter);
 
 module.exports = server;
-
-
